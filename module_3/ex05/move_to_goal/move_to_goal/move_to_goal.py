4b114358d770cdb66864ceee9afab35a34f0f12c
pritor <petya.podstavkin@mail.ru>
2023-10-04 15:15:43 +0700

    all working ex05

--- module_3/ex05/move_to_goal/move_to_goal/move_to_goal.py
+++ module_3/ex05/move_to_goal/move_to_goal/move_to_goal.py
@@ -7,9 +7,9 @@ from rclpy.node import Node
 import numpy as np
 
 class MoveToGoal(Node):
-    def __init__(self, goal_x, goal_y):
+    def __init__(self, goal_x, goal_y, goal_theta):
         super().__init__('move_to_goal')
-        self.goal_x, self.goal_y = goal_x, goal_y
+        self.goal_x, self.goal_y, self.goal_theta = goal_x, goal_y, goal_theta
         self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.check_pose_callback, 10)
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
@@ -17,19 +17,28 @@ class MoveToGoal(Node):
     def check_pose_callback(self, msg):
 
         vel = Twist()
-        x_error = self.goal_x - msg.x
-        y_error = self.goal_y - msg.y
+        x_error = round(self.goal_x - msg.x, 4)
+        y_error = round(self.goal_y - msg.y, 4)
         goal_theta = np.arctan2(y_error, x_error)
-        if abs(x_error)<0.2 and abs(y_error)<0.2:
+        s = np.sqrt(x_error**2 + y_error**2)
+        if s < 0.02:
+            goal_theta = self.goal_theta*np.pi/180
+
+            theta_error = goal_theta - msg.theta
+            vel.angular.z = theta_error
+            vel.linear.x = 0.0
+            self.get_logger().info(f"i got:{self.goal_x} {self.goal_y} {msg.x} {msg.y} {msg.theta * 180 / np.pi}\n "
+                                   f"i counted: {theta_error * 180 / np.pi} {x_error} {y_error}\n {goal_theta * 180 / np.pi}")
+            self.publisher_.publish(vel)
             self.destroy_node()
             rclpy.shutdown()
         theta_error = goal_theta - msg.theta
-        self.get_logger().info(f"i got:{self.goal_x} {self.goal_y} {msg.x} {msg.y}\n i counted: {theta_error*180/np.pi} {x_error} {y_error}")
+        self.get_logger().info(f"i got:{self.goal_x} {self.goal_y} {msg.x} {msg.y} {msg.theta*180/np.pi}\n "
+                               f"i counted: {theta_error*180/np.pi} {x_error} {y_error}\n {goal_theta*180/np.pi}")
 
-        vel.angular.z = 1.5 * theta_error
-        vel.linear.x = 1.0
+        vel.angular.z = 6 * theta_error
+        vel.linear.x = 1.5
         self.publisher_.publish(vel)
-
 
 
 
@@ -38,7 +47,7 @@ class MoveToGoal(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    move_to_goal = MoveToGoal(7, 3)
+    move_to_goal = MoveToGoal(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]))
 
     rclpy.spin(move_to_goal)
 
